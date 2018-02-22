79db6be3776ff9e492a50d599193dc61c183a1b5
SSadatmansoori <36413802+SSadatmansoori@users.noreply.github.com>
2018-02-22 10:32:01 -0500

    Add files via upload

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,16 @@
+public class LetterToYourself 
+{
+    public static void main(String[] args) {
+        System.out.println("+--------------------------------------------+");
+        System.out.println("|                                        ####|");
+        System.out.println("|                                        ####|");
+        System.out.println("|                                        ####|");
+        System.out.println("|                                            |");
+        System.out.println("|               Shaweon Sadatmansoori        |");
+        System.out.println("|               20 Bridle Pathway            |");
+        System.out.println("|               Ontario,CA 28037             |");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+        System.out.println("+--------------------------------------------+");
+    }
+}
