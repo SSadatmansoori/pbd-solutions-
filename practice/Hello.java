03b3271e41134c34d6087e08e278808fa7457f14
SSadatmansoori <36413802+SSadatmansoori@users.noreply.github.com>
2018-03-23 10:34:14 -0400

    Add files via upload

---
+++ practice/Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+    public static void main(String[] args) {
+    
+    int x = 0;
+    while (x < 5) {
+        System.out.println("Hello"); 
+        x += 1; 
+    } 
+    
+    System.out.println("-----------"); 
+    
+    
+    for (int y = 0; y < 5; y += 1) {
+        System.out.println("Hello from for loop");
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   }
+}
+  
